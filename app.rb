6edeb77cb1733bb3915e71976940b6c2510dcafb
sdzubiria <34401013+sdzubiria@users.noreply.github.com>
2024-04-17 21:04:00 +0000

    Final draft

--- app.rb
+++ app.rb
@@ -13,12 +13,12 @@ get("/square/new") do
 end
 
 get("/square/results") do
-  @the_num = params.fetch("users_number").to_i
-  
-  @the_result = params.fetch("users_number").to_f ** 2
+  user_input = params.fetch("users_number").to_f
+  @the_num = user_input
+  @the_result = user_input ** 2
   erb(:square_results)
-
 end
+
 
 get("/square_root/new") do
   erb(:new_square_root_calc)
@@ -27,11 +27,10 @@ end
 
 get("/square_root/results") do
   @the_num = params.fetch("users_number").to_f
-  
   @the_result = Math.sqrt(@the_num)
   erb(:square_root_results)
-
 end
+
 
 get("/payment/new") do
   erb(:new_payment)
